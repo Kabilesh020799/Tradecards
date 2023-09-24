0c2a628cd56d1858045b7fcccc5546acb17883fa
Kabilesh <kabilesh@dal.ca>
2023-09-24 14:30:44 -0300

    check deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome to Trade Cards
+        Welcome to Trade Cards Testing
       </header>
     </div>
   );
