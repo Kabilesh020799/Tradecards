fc00b06499c4d3165dc78e729ca46a93c2d4ce10
Kabilesh <kabilesh@dal.ca>
2023-09-28 15:42:04 -0300

    Setup: Removed netlify deployement support

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome to Trade Cards Testing12
+        Welcome to Trade Cards Testing
       </header>
     </div>
   );
