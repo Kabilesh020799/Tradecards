c68cd4f329bf6ea7ba1ee5bbd685dca53e8e02de
Kabilesh <kabilesh@dal.ca>
2023-10-23 12:13:24 -0300

    feature: coupon listing implemented

--- frontend/tradecards_ui/src/containers/home/home.jsx
+++ frontend/tradecards_ui/src/containers/home/home.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import NavBar from '../../components/nav-bar';
 import Slideshow from '../../components/banner/banner';
+import CouponListing from '../../components/coupon-listing';
 
 const Home = (props) => {
   const slides = [
     {
-      image: 'https://via.placeholder.com/800x400',
+      image: 'https://seasidefm.com/wp-content/uploads/2023/10/fall-Dance-4.png',
       caption: 'Slide 1',
     },
     {
-      image: 'https://via.placeholder.com/800x400',
+      image: 'https://discoverhalifaxns.com/wp-content/uploads/2023/08/88123_Event_Image_fe0e4e08-76a3-46a0-9063-5a3a84e0aee6.jpg',
       caption: 'Slide 2',
     },
     // Add more slides as needed
@@ -18,6 +19,9 @@ const Home = (props) => {
     <div>
       <NavBar />
       <Slideshow slides={slides}/>
+      <div className='coupon-listing'>
+        <CouponListing />
+      </div>
     </div>
   );
 };
