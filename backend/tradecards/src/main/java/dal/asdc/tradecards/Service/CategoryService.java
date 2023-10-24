815b8b9c4e152908d378d7635c8a27c06c330fed
harshpreet08 <hr605358@dal.ca>
2023-10-24 20:41:22 -0300

    removed create-category api

--- backend/tradecards/src/main/java/dal/asdc/tradecards/Service/CategoryService.java
+++ backend/tradecards/src/main/java/dal/asdc/tradecards/Service/CategoryService.java
@@ -1,13 +1,10 @@
 package dal.asdc.tradecards.Service;
 
 import dal.asdc.tradecards.Model.DAO.CategoryDao;
-import dal.asdc.tradecards.Model.DTO.CategoryDTO;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public interface CategoryService {
     public List<CategoryDao> getAllCategories();
-
-    public CategoryDao createCategory(CategoryDTO categoryDTO);
 }
--- backend/tradecards/src/main/java/dal/asdc/tradecards/Service/impl/CategoryServiceImpl.java
+++ backend/tradecards/src/main/java/dal/asdc/tradecards/Service/impl/CategoryServiceImpl.java
@@ -1,7 +1,6 @@
 package dal.asdc.tradecards.Service.impl;
 
 import dal.asdc.tradecards.Model.DAO.CategoryDao;
-import dal.asdc.tradecards.Model.DTO.CategoryDTO;
 import dal.asdc.tradecards.Repository.CategoryRepository;
 import dal.asdc.tradecards.Service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +14,5 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<CategoryDao> getAllCategories(){
         return (List<CategoryDao>)categoryRepository.findAll();
-    }
-
-    public CategoryDao createCategory(CategoryDTO categoryDTO){
-        CategoryDao category = new CategoryDao();
-        category.setCategoryName(categoryDTO.getCategoryName());
-        try {
-            CategoryDao savedCategory = categoryRepository.save(category);
-            return savedCategory;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
     }
 }
