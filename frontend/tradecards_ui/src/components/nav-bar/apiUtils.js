b65015c6aec0aa7888d1cca6f3792de6e055ac02
Kabilesh <kabilesh@dal.ca>
2023-11-12 22:11:35 -0400

    bug: prod env change

--- frontend/tradecards_ui/src/components/nav-bar/apiUtils.js
+++ frontend/tradecards_ui/src/components/nav-bar/apiUtils.js
@@ -3,7 +3,7 @@ import { getStorage } from '../../common-utils';
 const user = JSON.parse(getStorage('userInfo'));
 
 const getCategories = () => {
-  return fetch(process.env.REACT_APP_END_POINT + '/api/categories', { headers: { Authorization: `Bearer ${user.token}`, }, })
+  return fetch(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT + '/api/categories', { headers: { Authorization: `Bearer ${user.token}`, }, })
     .then((res) => res.json())
     .then((res) => res.filter((resItem) => resItem?.categoryName));
 };
