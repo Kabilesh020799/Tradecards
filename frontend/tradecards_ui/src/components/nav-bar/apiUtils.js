b29f4366411ca04463763281313dbd44eb7dbda6
Kabilesh <kabilesh@dal.ca>
2023-11-13 18:38:12 -0400

    bufix: Endpoint chanfge

--- frontend/tradecards_ui/src/components/nav-bar/apiUtils.js
+++ frontend/tradecards_ui/src/components/nav-bar/apiUtils.js
@@ -1,9 +1,11 @@
 import { getStorage } from '../../common-utils';
 
 const user = JSON.parse(getStorage('userInfo'));
+// const REACT_APP_END_POINT = 'http://localhost:8080';
+const REACT_APP_END_POINT_PROD = 'http://csci5308vm13.research.cs.dal.ca';
 
 const getCategories = () => {
-  return fetch((process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT) + '/api/categories', { headers: { Authorization: `Bearer ${user.token}`, }, })
+  return fetch(REACT_APP_END_POINT_PROD + '/api/categories', { headers: { Authorization: `Bearer ${user.token}`, }, })
     .then((res) => res.json())
     .then((res) => res.filter((resItem) => resItem?.categoryName));
 };
--- frontend/tradecards_ui/src/containers/coupon-detail/apiUtils.js
+++ frontend/tradecards_ui/src/containers/coupon-detail/apiUtils.js
@@ -1,15 +1,17 @@
 import { getStorage } from '../../common-utils';
 
 const user = JSON.parse(getStorage('userInfo'));
+// const REACT_APP_END_POINT = 'http://localhost:8080';
+const REACT_APP_END_POINT_PROD = 'http://csci5308vm13.research.cs.dal.ca';
 
 const getCouponDetail = (id) => {
   console.log(process.env.NODE_ENV);
-  return fetch((process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT) + '/api/coupon/get-coupon/' + id, { headers: { Authorization: `Bearer ${user.token}`, }, })
+  return fetch(REACT_APP_END_POINT_PROD + '/api/coupon/get-coupon/' + id, { headers: { Authorization: `Bearer ${user.token}`, }, })
     .then((res) => res.json());
 };
 
 const getUserDetail = (id) => {
-  return fetch((process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT) + `/api/users/details/${id}`,
+  return fetch(REACT_APP_END_POINT_PROD + `/api/users/details/${id}`,
     { headers: { Authorization: `Bearer ${user.token}`, }, })
     .then((res) => res.json());
 };
