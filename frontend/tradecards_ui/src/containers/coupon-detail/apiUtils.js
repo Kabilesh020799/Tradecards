4e658a69bdb34ac37dff67ff612b881337b4bc38
Kabilesh <kabilesh@dal.ca>
2023-11-12 22:20:09 -0400

    bug: env fix

--- frontend/tradecards_ui/src/containers/coupon-detail/apiUtils.js
+++ frontend/tradecards_ui/src/containers/coupon-detail/apiUtils.js
@@ -3,12 +3,13 @@ import { getStorage } from '../../common-utils';
 const user = JSON.parse(getStorage('userInfo'));
 
 const getCouponDetail = (id) => {
-  return fetch(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT + '/api/coupon/get-coupon/' + id, { headers: { Authorization: `Bearer ${user.token}`, }, })
+  console.log(process.env.NODE_ENV);
+  return fetch((process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT) + '/api/coupon/get-coupon/' + id, { headers: { Authorization: `Bearer ${user.token}`, }, })
     .then((res) => res.json());
 };
 
 const getUserDetail = (id) => {
-  return fetch(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT + `/api/users/details/${id}`,
+  return fetch((process.env.NODE_ENV === 'production' ? process.env.REACT_APP_END_POINT_PROD : process.env.REACT_APP_END_POINT) + `/api/users/details/${id}`,
     { headers: { Authorization: `Bearer ${user.token}`, }, })
     .then((res) => res.json());
 };
