47b87125acce2d73b03f8b4efcfaef9e7d4e64d1
Parth Modi <pr571545@dal.ca>
2023-11-18 17:13:14 -0400

    endpoint fix

--- backend/tradecards/src/main/java/dal/asdc/tradecards/Config/SecurityConfig.java
+++ backend/tradecards/src/main/java/dal/asdc/tradecards/Config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors(request -> new CorsConfiguration().addAllowedOrigin("*"))
                 .csrf(c -> c.disable())
                 .authorizeHttpRequests(
                         auth->auth.requestMatchers(
