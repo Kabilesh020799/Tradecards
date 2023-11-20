ca55ecdf7e028ffe578214e9e2fb60a22fcff9a5
Kabilesh <kabilesh@dal.ca>
2023-11-20 18:31:39 -0400

    bug: design fixes

--- frontend/tradecards_ui/src/containers/login/apiUtils.js
+++ frontend/tradecards_ui/src/containers/login/apiUtils.js
@@ -1,5 +1,5 @@
-// const REACT_APP_END_POINT = 'http://localhost:8080';
-const REACT_APP_END_POINT_PROD = 'http://csci5308vm13.research.cs.dal.ca:8080';
+const REACT_APP_END_POINT_PROD = 'http://localhost:8080';
+// const REACT_APP_END_POINT_PROD = 'http://csci5308vm13.research.cs.dal.ca:8080';
 
 const onLogin = (userName, password) => {
   const data = {
